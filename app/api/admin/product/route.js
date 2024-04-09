c240d701d856c25bc3a1d3dd09814b9d5fed7c24
tanmoypaul007 <genie.tanmoypaul@gmail.com>
2024-04-10 00:02:41 +0600

    Commented out unused code in product route files

--- app/api/admin/product/route.js
+++ app/api/admin/product/route.js
@@ -1,21 +1,21 @@
-import Product from "../../../../models/Product";
-import connectMongo from "@/util/db";
+// import Product from "../../../../models/Product";
+// import connectMongo from "@/util/db";
 
-export async function POST(request) {
-    try {
-        const new_product = await request.json();
+// export async function POST(request) {
+//     try {
+//         const new_product = await request.json();
 
-       await connectMongo();
-        const product = new Product({
-            ...new_product,
-            price: parseInt(new_product.price)
-        });
+//        await connectMongo();
+//         const product = new Product({
+//             ...new_product,
+//             price: parseInt(new_product.price)
+//         });
 
-        await product.save();
-        return Response.json({ message: "Product added successfully" });
+//         await product.save();
+//         return Response.json({ message: "Product added successfully" });
 
-    } catch (err) {
-        console.error(err);
-        return Response.json({ message: "Internal Server Error" });
-    }
-};
+//     } catch (err) {
+//         console.error(err);
+//         return Response.json({ message: "Internal Server Error" });
+//     }
+// };
