9e4d7b3bc0bd851f1d136714035261a9b2b30846
tanmoypaul007 <genie.tanmoypaul@gmail.com>
2024-04-09 23:44:53 +0600

    Update import paths in product routes

--- app/api/admin/product/route.js
+++ app/api/admin/product/route.js
@@ -1,4 +1,4 @@
-import Product from "@/models/Product";
+import Product from "../../../../models/Product";
 import connectMongo from "@/util/db";
 
 export async function POST(request) {
--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,9 +1,9 @@
 export const dynamic = 'force-dynamic' // defaults to auto
-import Product from '@/models/Product';
+import Product from '../../../models/Product';
 import connectMongo from '@/util/db';
 
 export async function GET() {
-  
+
   try {
     await connectMongo();
     let products = await Product.find({});
