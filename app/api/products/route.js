87e08396c0964b8571088bc0461512fc5c2295a2
tanmoypaul007 <genie.tanmoypaul@gmail.com>
2024-04-11 18:32:04 +0600

    Refactor product route to fetch and return products

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,18 +1,18 @@
-// export const dynamic = 'force-dynamic' // defaults to auto
-// import Product from '../../../models/Product';
-// import connectMongo from '@/util/db';
+export const dynamic = 'force-dynamic' // defaults to auto
+import Product from '../../../models/Product';
+import connectMongo from '@/util/db';
 
-// export async function GET() {
+export async function GET() {
 
-//   try {
-//     await connectMongo();
-//     let products = await Product.find({});
-//     return Response.json({ status: 200, data: products, message: "Product is Found" });
-//   } catch (err) {
-//     console.error(err);
-//     return Response.json({ message: "Internal Server Error" });
-//   }
-// }
+  try {
+    await connectMongo();
+    let products = await Product.find({});
+    return Response.json({ status: 200, data: products, message: "Product is Found" });
+  } catch (err) {
+    console.error(err);
+    return Response.json({ message: "Internal Server Error" });
+  }
+}
 
 
 
