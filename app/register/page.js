825f7743bdf3cc7641979fc882647d9942474dca
tanmoypaul007 <genie.tanmoypaul@gmail.com>
2024-04-16 18:15:40 +0600

    Fix styling and add input field for name

--- app/register/page.js
+++ app/register/page.js
@@ -3,8 +3,8 @@ import Image from 'next/image'
 
 const Register = () => {
     return (
-        <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-            <div className="max-w-screen-xl m-0 sm:m-10 bg-slate-800 text-white shadow sm:rounded-lg flex justify-center flex-1">
+        // <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+            <div className="w-full m-0 sm:m-10 bg-slate-800 text-white shadow sm:rounded-lg flex justify-center flex-1">
                 <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
                     <div>
                         {/* <img src="https://storage.googleapis.com/devitary-image-host.appspot.com/15846435184459982716-LogoMakr_7POjrN.png"
@@ -60,15 +60,21 @@ const Register = () => {
                                 </div>
                             </div>
 
-                            <div className="mx-auto max-w-xs">
+                            <div className="mx-auto max-w-xs space-y-5">
+
+                                <input
+                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                                    type="email" placeholder="Name" />
+
                                 <input
                                     className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
                                     type="email" placeholder="Email" />
                                 <input
-                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                                    className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
                                     type="password" placeholder="Password" />
+
                                 <button
-                                    className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                                    className=" tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
                                     <svg className="w-6 h-6 -ml-2" fill="none" stroke="currentColor" stroke-width="2"
                                         stroke-linecap="round" stroke-linejoin="round">
                                         <path d="M16 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2" />
@@ -79,7 +85,7 @@ const Register = () => {
                                         Sign Up
                                     </span>
                                 </button>
-                                <p className="mt-6 text-xs text-center">
+                                <p className=" text-xs text-center">
                                     I agree to abide by templatanas
                                     <a href="#" className="border-b border-gray-500 border-dotted">
                                         Terms of Service
@@ -93,14 +99,14 @@ const Register = () => {
                         </div>
                     </div>
                 </div>
+
                 <div className="flex-1 bg-slate-800 text-center hidden lg:flex">
                     <div className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
                         style={{ backgroundImage: "url('https://storage.googleapis.com/devitary-image-host.appspot.com/15848031292911696601-undraw_designer_life_w96d.svg')" }}>
                     </div>
-             
                 </div>
             </div>
-        </div>
+        // </div>
     )
 }
 
