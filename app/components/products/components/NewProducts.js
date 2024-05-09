ebf8af5e279080613834b066a32554411786f26a
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-05-09 09:23:45 +0600

    refactor: Remove unused API calls and commented code

--- app/components/products/components/NewProducts.js
+++ app/components/products/components/NewProducts.js
@@ -7,19 +7,19 @@ import { kuProductList } from '@/util/url'
 
 const NewProducts = async () => {
 
-    // const products = await fetch(base_url + kuProductList,{ next: { revalidate: 1 } })
-    //     .then(res => res.json())
+    const products = await fetch(base_url + kuProductList,{ next: { revalidate: 1 } })
+        .then(res => res.json())
 
     return (
 
         <div className="newProductsContainer">
 
             <section className="grid justify-center grid-cols-1 mx-auto mt-10 mb-5 w-fit lg:grid-cols-3 md:grid-cols-2 justify-items-center gap-y-20 gap-x-14">
-                {/* {
+                {
                     products?.data?.map((product, index) => (
                         <ProductCard key={index} product={product}  />
                     ))
-                } */}
+                }
             </section>
         </div>
 
