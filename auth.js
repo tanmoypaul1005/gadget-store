f14f7b8c3f5e4c0c5bc3dbf78c097742a7265267
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-05-29 19:48:15 +0600

    refactor: Update login form submission logic and error handling

--- auth.js
+++ auth.js
@@ -1,17 +1,14 @@
+
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import User from "./models/User";
 import mongoClientPromise from "./util/mongoClientPromise";
+import User from "./models/User";
 
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
+
+export default NextAuth({
   adapter: MongoDBAdapter(mongoClientPromise, {
     databaseName: process.env.ENVIRONMENT,
   }),
@@ -19,9 +16,38 @@ export const {
     strategy: "jwt",
   },
   providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials) {
+        if (!credentials) return null;
+        try {
+          const user = await User.findOne({ email: credentials.email });
+          console.log({ user });
+          if (user) {
+            const isMatch = await bcrypt.compare(
+              credentials.password,
+              user.password
+            );
+            if (isMatch) {
+              return user;
+            } else {
+              throw new Error("Email or password mismatch");
+            }
+          } else {
+            throw new Error("User not found");
+          }
+        } catch (error) {
+          throw new Error(error);
+        }
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 });
+
