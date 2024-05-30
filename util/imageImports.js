59d316e2e88014821397cde9bc211f6a9a4a4b5c
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-05-30 21:50:51 +0600

    refactor: Remove unused video import in imageImports.js

--- util/imageImports.js
+++ util/imageImports.js
@@ -1,3 +1,2 @@
 export { default as iPasswordHide } from '../public/images/icons/password-hide.png';
 export { default as iPasswordShow } from '../public/images/icons/password-show.png';
-export { default as vBanner } from '../public/video/banner.mp4';
