96b62b2e22d86471294d560780ee58ef7e162c84
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-05-31 13:21:38 +0600

    refactor: Add lazy loading for ProductCard component in Category page

--- app/category/[category_id]/page.js
+++ app/category/[category_id]/page.js
@@ -1,6 +1,7 @@
 export const revalidate = 10;
 import { getByCategoryProducts } from "@/app/action/category";
 import ProductBox from "@/app/components/products/components/ProductBox";
+import ProductCard from "@/app/components/products/components/ProductCard";
 import { base_url } from "@/util/const";
 import React from "react";
 
@@ -10,10 +11,10 @@ const Category = async ({ params }) => {
   console.log("products", products);
 
   return (
-    <div className="flex justify-between">
+    <div className="flex space-x-10 mx-[40px] pb-[40px]">
       {products?.data?.map((product, index) => (
-        <div className="w-[400px] h-[600px]" key={index}>
-        <ProductBox key={index} product={product} />
+        <div className="" key={index}>
+        <ProductCard key={index} product={product} />
         </div>
       ))}
     </div>
