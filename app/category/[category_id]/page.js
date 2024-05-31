291d03785e330e7f6d9fac4684b2b38650e198b0
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-05-31 15:27:27 +0600

    refactor: Update Category component import to use sidebar folder

--- app/category/[category_id]/page.js
+++ app/category/[category_id]/page.js
@@ -1,14 +1,11 @@
 export const revalidate = 10;
 import { getByCategoryProducts } from "@/app/action/category";
-import ProductBox from "@/app/components/products/components/ProductBox";
 import ProductCard from "@/app/components/products/components/ProductCard";
-import { base_url } from "@/util/const";
 import React from "react";
 
 const Category = async ({ params }) => {
-  console.log("params", params);
+
   const products = await getByCategoryProducts(params?.category_id);
-  console.log("products", products);
 
   return (
     <div className="flex space-x-10 mx-[40px] pb-[40px]">
