75d2f3f3bc94eca3b0b6bccc5a68e6755ee85976
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-06-04 07:58:12 +0600

    chore: Update npm dependencies and add new packages for lazy loading images and carousel functionality

--- app/checkout/components/ClearCart.js
+++ app/checkout/components/ClearCart.js
@@ -1,23 +1,31 @@
 "use client";
 import { deleteCart } from "@/app/action/cart";
 import { Toastr } from "@/util/utilityFunction";
+import Image from "next/image";
 import React from "react";
 
 const ClearCart = (params) => {
 
   return (
     <div
-      className="cursor-pointer"
+      className="cursor-pointer flex justify-center items-center"
       onClick={async () => {
-      const success=  await deleteCart(params?.data,"/checkout");
-      if(success){
-        Toastr({ message: "Cart Cleared Successfully", type: "success" })
-      }else{
-        Toastr({ message: "Cart Cleared Failed", type: "error" })
-      }
+        const success = await deleteCart(params?.data, "/checkout");
+        if (success) {
+          Toastr({ message: "Cart Cleared Successfully", type: "success" })
+        } else {
+          Toastr({ message: "Cart Cleared Failed", type: "error" })
+        }
       }}
     >
-      Clear
+      <Image
+        style={{ maxWidth: "25", minWidth: "25px", naxHeight: "25px", minHeight: "25px", cursor: "pointer" }}
+        className=' cursor-pointer'
+        src={"/images/icons/redCancel.svg"}
+        alt=''
+        width={10}
+        height={10}
+      />
     </div>
   );
 };
--- app/components/home/Banner.js
+++ app/components/home/Banner.js
@@ -1,22 +1,84 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
 import Image from "next/image";
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
+
+import "./Carousel.css";
+
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Banner() {
 
+
+  const data = [
+    {
+      "src": "https://picsum.photos/seed/img1/600/400",
+      "alt": "Image 1 for carousel"
+    },
+    {
+      "src": "https://picsum.photos/seed/img2/600/400",
+      "alt": "Image 2 for carousel"
+    },
+    {
+      "src": "https://picsum.photos/seed/img3/600/400",
+      "alt": "Image 3 for carousel"
+    }
+  ]
+
+
+
+  const [slide, setSlide] = useState(0);
+
+  
+useEffect(() => {
+  const timer = setInterval(() => {
+    setSlide(slide => slide === data.length - 1 ? 0 : slide + 1);
+  }, 3000); // Change slide every 3 seconds
+
+  // Cleanup function to clear the timer when the component unmounts
+  return () => clearInterval(timer);
+}, []); // Empty dependency array means this effect runs once on mount and cleanup on unmount
+
+
+  const nextSlide = () => {
+    setSlide(slide === data.length - 1 ? 0 : slide + 1);
+  };
+
+  const prevSlide = () => {
+    setSlide(slide === 0 ? data.length - 1 : slide - 1);
+  };
+
+
   return (
-    <div className="relative flex items-center justify-center mx-auto mt-10 rounded lg:w-3/4 banner lg:-mt-4">
-      <Image
-        style={{ maxHeight: "550px", }}
-        className="rounded-lg mt-5"
-        src={"/images/banner-1.png"}
-        width={500}
-        height={400}
-        alt=""
+    <div className="relative flex justify-center items-center mx-auto mt-10 lg:h-[500px] rounded lg:w-3/4  banner lg:-mt-4 w-full h-64 sm:h-96 md:h-128 ">
+      {data.map((item, idx) => {
+        return (
+          <img
+            src={item.src}
+            alt={item.alt}
+            key={idx}
+            className={slide === idx ? "rounded-lg shadow-md w-full h-full" : "hidden"}
+          />
+        );
+      })}
+      <BsArrowRightCircleFill
+        onClick={nextSlide}
+        className="absolute filter drop-shadow-md w-8 h-8 text-white hover:cursor-pointer right-4"
       />
-      {/* <div className="rounded cursor-pointer absolute pl-3 flex flex-col items-start justify-center w-full h-full text-black  ">
-        <h1 className="text-3xl font-bold text-center lg:text-4xl">New Collection</h1>
-        <p className="text-base font-semibold">Get the latest collection of shoes.</p>
-        <button className="px-4 py-2 mt-4 text-center text-white bg-gray-900 rounded-md hover:bg-red-500">SHOP NOW</button>
-      </div> */}
+      <span className="absolute bottom-4 flex">
+        {data.map((_, idx) => {
+          return (
+            <button
+              key={idx}
+              className={
+                slide === idx ? "bg-white h-2 w-2 rounded-full border-none outline-none shadow-md mx-1 cursor-pointer" : "bg-gray-500 h-2 w-2 rounded-full border-none outline-none shadow-md mx-1 cursor-pointer"
+              }
+              onClick={() => setSlide(idx)}
+            ></button>
+          );
+        })}
+      </span>
     </div>
   );
 }
