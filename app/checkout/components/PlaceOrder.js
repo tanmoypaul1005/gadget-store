b9fca0f534b0c3241c3030403765152db30cd2f4
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-06-07 12:56:34 +0600

    refactor: Update import paths and remove unused constants in PlaceOrder component

--- app/checkout/components/PlaceOrder.js
+++ app/checkout/components/PlaceOrder.js
@@ -1,4 +1,8 @@
 "use client"
+
+import { addOrder } from "@/app/action/order";
+import { Toastr } from "@/util/utilityFunction";
+
 const PlaceOrder = ({ address, email,totalPrice=0 }) => {
 
     const shipping_address = address?.find(
@@ -9,14 +13,20 @@ const PlaceOrder = ({ address, email,totalPrice=0 }) => {
     );
 
 
-    const handleOrder = () => {
+    const handleOrder =async () => {
         const body = {
             email: email,
             shipping_address: shipping_address._id,
             billing_address: billing_address._id,
             total_amount: totalPrice,
             items: cart.map((c) => ({ cart: c._id })),
+        }
 
+        const response = await addOrder(body);
+        if(response?.success){
+            Toastr({message:"Order placed successfully",type:"success"})
+        }else{
+            Toastr({message:"Something went wrong",type:"success"})
         }
     }
 
