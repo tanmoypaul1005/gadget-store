7e5ff0dd30365368b648bc5f80527a0307ada348
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-06-09 00:06:36 +0600

    refactor: Update comment field name in Comment model

--- models/Comment.js
+++ models/Comment.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
 
-    title: {
+    comment: {
         type: String,
         required: true,
         trim: true
