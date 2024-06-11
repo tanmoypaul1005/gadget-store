d3434cad3b1d590070336ab8b26a94253235db6d
tanmoypaul007 <genie.tanmoypaul@gmail.com>
2024-06-11 10:56:07 +0600

    Refactor layout and featured categories components

--- app/components/FeaturedCategories.js
+++ app/components/FeaturedCategories.js
@@ -11,14 +11,12 @@ const FeaturedCategories = async () => {
 
     return (
         <div>
-
-            <div className='flex flex-wrap justify-between gap-2'>
+            <div className='grid grid-cols-1 gap-y-4 gap-x-4 3xl:gap-x-14 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 2xl:grid-cols-7'>
                 {
                     category?.data?.map((item, index) => (
                         <CategoryBox item={item} key={index} />
                     ))
                 }
-
             </div>
         </div>
     )
