e83f47cb588f8fad6a1ecd4f29b9ef3a9e06a695
tanmoypaul007 <genie.tanmoypaul@gmail.com>
2024-06-12 16:28:45 +0600

    Commented out unused code

--- app/products/[product_id]/page.js
+++ app/products/[product_id]/page.js
@@ -20,9 +20,9 @@ const ProductDetails = async ({ params }) => {
 
   const product = await response?.json();
   const productDetails = product?.data;
-  const session = await auth();
+  // const session = await auth();
 
-  const user = await findUserId(session?.user?.email);
+  // const user = await findUserId(session?.user?.email);
 
   return (
     <div className="common-class">
