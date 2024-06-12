c42cb109841c60fe721da839a904822bf950fb4c
tanmoypaul007 <genie.tanmoypaul@gmail.com>
2024-06-12 13:15:25 +0600

    Add useDebounce hook to improve search functionality

--- app/components/header/Search.jsx
+++ app/components/header/Search.jsx
@@ -1,19 +1,23 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 
 import React, { useEffect, useState } from 'react'
+import { useDebounce } from 'use-debounce';
 
 const Search = () => {
 
     const [searchTerm, setSearchTerm] = useState('');
     const [searchResults, setSearchResults] = useState([]);
 
+    const [searchValue] = useDebounce(searchTerm, 500);
+
     useEffect(() => {
         fetchData();
-    }, [searchTerm]);
+    }, [searchValue]);
 
     const fetchData = async () => {
-        if (searchTerm) {
-            fetch(`http://localhost:3000/api/search?query=${searchTerm}`)
+        if (searchValue) {
+            fetch(`http://localhost:3000/api/search?query=${searchValue}`)
                 .then(response => response.json())
                 .then(data => setSearchResults(data));
         } else {
@@ -22,24 +26,28 @@ const Search = () => {
     }
 
     return (
-        <div>
+        <div className='w-full'>
             <input
                 value={searchTerm}
                 onChange={event => setSearchTerm(event.target.value)}
                 id="email"
                 name="email"
-                className={`w-full  text-white bg-cCommonBg  rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500`}
+                className={`w-full text-white bg-cCommonBg  rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500`}
                 placeholder={"Search for products, brands and categories"}
             />
-            {searchResults?.data?.length > 0 && (
-                <div className="dropdown">
-                    {searchResults?.data?.map((result, index) => (
-                        <div key={index} className="dropdown-item">
-                            {result.name}
+            {
+                searchResults?.data?.length > 0 && (
+                    <div className="relative">
+                        <div className="absolute z-10 w-full mt-2 rounded-md shadow-lg bg-cCommonBg">
+                            {searchResults?.data?.map((result, index) => (
+                                <div key={index} className="px-4 py-2 cursor-pointer hover:bg-zinc-700">
+                                    {result.name}
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
-            )}
+                    </div>
+                )
+            }
         </div>
 
     )
