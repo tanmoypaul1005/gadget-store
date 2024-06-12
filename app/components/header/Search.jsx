fcf23446bcea1084314472bb2f2aab9219723d4e
tanmoypaul007 <genie.tanmoypaul@gmail.com>
2024-06-12 10:14:27 +0600

    Refactor fetch requests and update search input styling

--- app/components/header/Search.jsx
+++ app/components/header/Search.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const Search= () => {
+const Search = () => {
     return (
-<div className="flex  justify-center overflow-hidden ">
-
-</div>
+        <input
+          
+            // value={value}
+            // onChange={onChange}
+            // type={type}
+            id="email"
+            name="email"
+            className={`w-full  text-white bg-cCommonBg  rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500`}
+            placeholder={"Search for products, brands and categories"}
+        />
 
     )
 }
