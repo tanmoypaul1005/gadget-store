4be15e31b85688971c07435ae9e279cf1c0fd4a2
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-16 22:36:49 +0600

    refactor: Update Message component for Facebook Messenger chat integration

--- app/components/Message.js
+++ app/components/Message.js
@@ -1,18 +1,16 @@
-
 "use client"
-import React from 'react'
-import MessengerCustomerChat from "react-messenger-customer-chat";
+import React from 'react';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+
 const Message = () => {
     return (
         <div>
-            
-        <MessengerCustomerChat
-          pageId="61564524985977"
-          appId="1026625409124458"
-         
-        />
+            <MessengerCustomerChat
+                pageId="61564524985977"
+                appId="1026625409124458"
+            />
         </div>
-    )
+    );
 }
 
-export default Message
+export default Message;
