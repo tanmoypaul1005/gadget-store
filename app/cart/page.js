7270d779a4b72a954e107d3e00346e373e07b7fb
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-18 09:01:35 +0600

    refactor: Update Cart page to use lazy loading for product images

--- app/cart/page.js
+++ app/cart/page.js
@@ -5,6 +5,7 @@ import { getCartCount } from "../action/cart";
 import Image from "next/image";
 import RemoveCart from "./components/RemoveCart";
 import Link from "next/link";
+import QuantityButton from "./components/QuantityButton";
 
 const Cart = async () => {
   const session = await auth();
@@ -34,11 +35,11 @@ const Cart = async () => {
                   </div>
 
                   <div className="flex flex-col">
-                    <h3 className="text-base font-bold text-white">
+                    <h3 className="text-base font-bold text-white line-clamp-2">
                       {item?.product?.name}
                     </h3>
                     <p className="text-xs font-semibold text-white mt-0.5">
-                      Qty:{item?.quantity}
+                      Qty:{item?.quantity ?? 0}
                     </p>
 
                     <button
@@ -69,33 +70,7 @@ const Cart = async () => {
                     ${item?.product?.price}
                   </h4>
 
-                  <button
-                    type="button"
-                    className="mt-6 flex items-center px-3 py-1.5 border border-gray-300 text-gwhite text-xs outline-none bg-transparent rounded-md"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-2.5 fill-current"
-                      viewBox="0 0 124 124"
-                    >
-                      <path
-                        d="M112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z"
-                        data-original="#ffff"
-                      ></path>
-                    </svg>
-
-                    <span className="mx-3 font-bold">2</span>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="w-2.5 fill-current"
-                      viewBox="0 0 42 42"
-                    >
-                      <path
-                        d="M37.059 16H26V4.941C26 2.224 23.718 0 21 0s-5 2.224-5 4.941V16H4.941C2.224 16 0 18.282 0 21s2.224 5 4.941 5H16v11.059C16 39.776 18.282 42 21 42s5-2.224 5-4.941V26h11.059C39.776 26 42 23.718 42 21s-2.224-5-4.941-5z"
-                        data-original="#000000"
-                      ></path>
-                    </svg>
-                  </button>
+                  <QuantityButton id={item?._id} initialQuantity={item?.quantity ?? 0}/>
                 </div>
               </div>
               {parseInt(cart?.length) - 1 !== parseInt(index) && (
@@ -228,14 +203,14 @@ const Cart = async () => {
                 Checkout
               </button>
             </Link>
-<div className="mt-3"></div>
-            <Link  href={"/"}>
-            <button
-              type="button"
-              className="text-sm px-4 py-2.5 w-full font-semibold tracking-wide bg-transparent text-gray-800 border border-gray-300 rounded-md"
-            >
-              Continue Shopping{" "}
-            </button>
+            <div className="mt-3"></div>
+            <Link href={"/"}>
+              <button
+                type="button"
+                className="text-sm px-4 py-2.5 w-full font-semibold tracking-wide bg-transparent text-gray-800 border border-gray-300 rounded-md"
+              >
+                Continue Shopping{" "}
+              </button>
             </Link>
           </div>
         </div>
