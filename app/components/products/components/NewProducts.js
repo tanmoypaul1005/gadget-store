69205bd1b9c9383da9f8fffdca3fbc140e00853a
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-20 13:00:12 +0600

    refactor: Add lazy loading for product images in NewProducts component

--- app/components/products/components/NewProducts.js
+++ app/components/products/components/NewProducts.js
@@ -1,13 +1,17 @@
 export const revalidate = 2; // revalidate at most every hour
 import React from "react";
 import ProductCard from "./ProductCard";
-import { base_url } from "@/util/const";
+import { base_url, products_type_value } from "@/util/const";
 import { kuProductList } from "@/util/url";
 
 const NewProducts = async () => {
   const products = await fetch(base_url + kuProductList, {
     next: { revalidate: 1 },
   }).then((res) => res.json());
+
+  const regularProducts = products?.data?.filter(
+    (product, index) => product.type === products_type_value.regular
+  );
 
   return (
     <div className="flex flex-col items-center justify-center">
@@ -16,7 +20,7 @@ const NewProducts = async () => {
       </div>
       <div className="newProductsContainer">
         <section className="flex sm:flex-row flex-col sm:justify-between justify-center items-center gap-x-[50px] flex-wrap justify-items-center gap-y-12">
-          {products?.data?.map((product, index) => (
+          {regularProducts?.map((product, index) => (
             <ProductCard key={index} product={product} />
           ))}
         </section>
