e359eba3cc4f63b992d92e782be67f77c0428dac
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-22 22:13:53 +0600

    refactor: Update CommentCard component to improve layout and performance
    - Remove commented out code for user avatar image
    - Update flex layout and spacing for better alignment
    - Use flex-col layout for mobile responsiveness
    - Adjust margin and padding for better spacing
    - Update text color and font styles
    - Add conditional rendering for comment title

--- app/products/[product_id]/components/CommentCard.js
+++ app/products/[product_id]/components/CommentCard.js
@@ -10,25 +10,12 @@ const CommentCard = async ({ comment, onCommentDelete }) => {
     return (
         <div>
             <div className="flex items-start my-8 space-x-4">
-                
-                        {/* <Image
-                          style={{
-                            maxWidth: "40px",
-                            minWidth: "40px",
-                            maxHeight: "40px",
-                            minHeight: "40px",
-                          }}
-                          className="rounded-full"
-                          src={user?.data?.image}
-                          alt="pic"
-                          width={20}
-                          height={20}
-                        /> */}
-                        <Avatar avatar={user?.data?.image ?? ""} name={user?.data?.name??""} />
-                      
+
+                <Avatar avatar={user?.data?.image ?? ""} name={user?.data?.name ?? ""} />
+
                 <div className="w-full">
 
-                    <div className='relative flex justify-between'>
+                    <div className='relative flex-col justify-between gap-y-2 md:flex-row'>
                         <h5 className="font-bold text-white"> {user?.data?.name}</h5>
                         <h5 className="font-normal text-white">{formatDate(comment?.createdAt)}</h5>
 
@@ -63,7 +50,7 @@ const CommentCard = async ({ comment, onCommentDelete }) => {
 
                     </div>
 
-                    <p className="text-slate-300">
+                    <p className="mt-2 text-slate-300 md:mt-0">
                         {comment?.comment ?? comment?.title}
                     </p>
                 </div>
