e8a41d3ab6677942f09c8e0f6e313e0bdd0438ce
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-22 19:03:53 +0600

    refactor: Update Card component to use CommonRating component for displaying product rating

--- app/products/components/Card.js
+++ app/products/components/Card.js
@@ -1,5 +1,6 @@
 "use client"
 import { serverAddCart } from "@/app/action/cart";
+import CommonRating from "@/components/CommonRating";
 import { Toastr } from "@/util/utilityFunction";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -37,26 +38,7 @@ const Card = ({ product }) => {
             {product?.name}
           </h5>
         <div className="my-2.5 flex items-center">
-          <span className="mr-2 rounded bg-yellow-200 px-2.5 py-0.5 text-xs font-semibold">
-            {parseInt(product?.ratting ?? 0)}.0
-          </span>
-          <div className="flex">
-            {
-              [...Array(product?.ratting)].map((item, index) => (
-                <div key={index}>
-                  <svg
-                    aria-hidden="true"
-                    className="w-5 h-5 text-yellow-300"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                  </svg>
-                </div>
-              ))
-            }
-          </div>
+          <CommonRating value={parseInt(product?.ratting ?? 0)}/>
         </div>
         <div className="flex items-center justify-between">
           <div className="text-base font-bold text-slate-900">${product?.price}</div>
