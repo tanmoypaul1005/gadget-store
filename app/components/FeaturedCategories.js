69a42410704f9944fd70cde29586e1a45ce3c18e
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-23 22:47:20 +0600

    refactor: Update FeaturedCategories component layout for responsive grid display

--- app/components/FeaturedCategories.js
+++ app/components/FeaturedCategories.js
@@ -7,11 +7,9 @@ const FeaturedCategories = async () => {
   const category = await getMainCategory();
 
   return (
-    <div className="flex flex-wrap justify-center gap-4 md:gap-6 lg:gap-x-12 lg:gap-y-6">
-      {category?.map((item, index) => (
-        <div className="flex justify-center" key={index}>
-          <CategoryBox item={item} />
-        </div>
+    <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 md:gap-6 lg:gap-8">
+      {category?.slice(0,10).map((item, index) => (
+        <CategoryBox item={item} key={index} />
       ))}
     </div>
   );
@@ -23,20 +21,18 @@ const CategoryBox = ({ item }) => {
   return (
     <Link
       href={`/category/featured/${item?._id}`}
-      className="bg-cCommonBg rounded flex-col
-        min-h-[108px] max-h-[108px] max-w-[108px] min-w-[108px]
-        sm:min-h-[145px] sm:max-h-[145px] sm:max-w-[145px] sm:min-w-[145px]
-        md:min-h-[170px] md:max-h-[170px] md:max-w-[170px] md:min-w-[170px]
-        flex justify-center items-center"
+      className="bg-cCommonBg rounded flex flex-col items-center justify-center
+        min-h-[100px] sm:min-h-[140px] md:min-h-[160px] lg:min-h-[180px] xl:min-h-[200px]
+        min-w-[100px] sm:min-w-[140px] md:min-w-[160px] lg:min-w-[180px] xl:min-w-[200px]"
     >
       <Image
-        className="w-[30px] h-[30px] sm:w-[65px] sm:h-[65px] md:w-[70px] md:h-[70px] lg:w-[75px] lg:h-[75px]"
-        width={75}
-        height={75}
+        className="w-[40px] h-[40px] sm:w-[60px] sm:h-[60px] md:w-[80px] md:h-[80px] lg:w-[100px] lg:h-[100px]"
+        width={100}
+        height={100}
         src={item?.icon}
         alt={item?.title || "Category Icon"}
       />
-      <div className="text-cDeepSaffron font-medium text-[12px] sm:text-[15px] md:text-[16px] lg:text-[17px] mt-3">
+      <div className="text-cDeepSaffron font-medium text-[12px] sm:text-[14px] md:text-[16px] lg:text-[18px] xl:text-[20px] mt-2 md:mt-3">
         {item?.title}
       </div>
     </Link>
