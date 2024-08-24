ad25b199dbb79f5e2a5791c4195e9459fc4f9d24
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-24 16:56:34 +0600

    refactor: Update grid layout in RegularProducts component for responsive display

--- app/category/featured/[category_id]/page.js
+++ app/category/featured/[category_id]/page.js
@@ -40,7 +40,7 @@ const FeaturedCategoriesDetails = async ({ params }) => {
       </div>
 
       {products?.data?.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           {products.data.map((product, index) => (
               <ProductCard key={index} product={product} />
           ))}
--- app/components/products/components/RegularProducts.js
+++ app/components/products/components/RegularProducts.js
@@ -27,8 +27,7 @@ const RegularProducts = async () => {
         </Link>
       </div>
 
-
-      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8">
+      <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {regularProducts?.map((product, index) => (
           <ProductCard key={index} product={product} />
         ))}
