f61a83aa0a790a426689402c6c31042e55a44bf0
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-25 18:07:01 +0600

    refactor: Convert offer_id to Mongoose ObjectId in getOfferProduct

--- app/offer/[offer_id]/page.js
+++ app/offer/[offer_id]/page.js
@@ -1,12 +1,18 @@
 import { getOfferProduct } from '@/app/action/offer';
+import Card from '@/app/products/components/Card';
 import React from 'react'
 
-const ProductDetails = async({params }) => {
+const ProductDetails = async({ params }) => {
     const product =await getOfferProduct(params?.offer_id);
     return (
         <div className='common-class common-topGap'>
-            {product?.length > 0 && product[0]?.name}
-            {params?.offer_id}
+           {
+                product && product.map((item, index) => {
+                    return (
+                        <Card product={item} key={index}/>
+                    )
+                })
+           }
         </div>
     )
 }
