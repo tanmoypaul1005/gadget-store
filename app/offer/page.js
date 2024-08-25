2f7d60a3b8bf9333bb29dca0ebfda0b003bfa7ac
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-25 12:48:00 +0600

    feat: Add new banner image for landing page

--- app/offer/page.js
+++ app/offer/page.js
@@ -1,13 +1,43 @@
 import React from 'react'
 import { getOffer } from '../action/offer';
+import Image from 'next/image';
+import { iFile } from '@/util/imageImports';
 
 const Offer = async() => {
 
     const offerList=await getOffer();
-    
+
     return (
-        <div>
-            
+        <div className='common-class common-topGap'>
+
+
+        <div className='flex flex-wrap items-center justify-between gap-y-5'>
+            {
+                offerList.map((offer,index)=>{
+                    return (
+                        <div className='rounded-md max-w-[300px] min-w-[300px]' key={index}>
+                            <Image style={{
+                                maxHeight: '300px',
+                                minHeight: '300px',
+                                maxWidth: '300px',
+                                minWidth: '300px'
+                            }} className='rounded-t-md' 
+                            src={iFile} alt={offer.title} height={352} width={352}  />
+                            <div className='p-3 space-y-2 rounded-b-md bg-[#e1e1e1] text-black'>
+
+                            <div className='text-base font-bold text-center line-clamp-2'>{offer.title}</div>
+                            <p className='text-sm font-semibold text-center line-clamp-2'>{offer.description}</p>
+                            <div className='flex items-center justify-center'>
+                                <button className='w-fit bg-[#f27f20] text-white  px-3 py-2  rounded-md'>View Offer</button>
+                                </div>
+                            </div>
+
+                            
+                        </div>
+                    )
+                })
+            }
+        </div>
         </div>
     )
 }
