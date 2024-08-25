d7d5e38a0874edca1e3f03eda98d788ae85cbe90
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-25 16:16:09 +0600

    refactor: Update Offer page to use offer thumbnail instead of iFile

---
+++ app/offer/[offer_id]/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductDetails = ({params }) => {
+    return (
+        <div>
+            {params?.offer_id}jjjj
+        </div>
+    )
+}
+
+export default ProductDetails
