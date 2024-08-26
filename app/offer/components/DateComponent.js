c4582d872f8ca547c08c0691901187e74af4c48d
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2024-08-26 13:00:49 +0600

    refactor: Update DateComponent to include a calendar icon and improve layout

--- app/offer/components/DateComponent.js
+++ app/offer/components/DateComponent.js
@@ -1,12 +1,16 @@
 import { formatDate } from '@/util/utilityFunction';
 import React from 'react'
+import { SlCalender } from "react-icons/sl";
 
 const DateComponent = () => {
     const currentDate = new Date();
     const nextMonthDate = new Date();
     nextMonthDate.setMonth(currentDate.getMonth() + 1);
     return (
-        <div className='text-xs font-medium text-center'>{`${formatDate(currentDate)} - ${formatDate(nextMonthDate)}`}</div>
+        <div className='flex items-center justify-center gap-x-2'>
+            <SlCalender />
+            <div className='text-xs font-medium text-center'>{`${formatDate(currentDate)} - ${formatDate(nextMonthDate)}`}</div>
+        </div>
     )
 }
 
