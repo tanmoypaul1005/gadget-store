f4570efb177b2a303bb632772dd3b25babccaf12
mugen-rahat-hosen <tanmoypaul1005@gmail.com>
2026-03-03 01:17:16 +0600

    fix: Remove commented-out getCartCount function from cartStore

--- stores/cartStore.js
+++ stores/cartStore.js
@@ -11,20 +11,4 @@ const useCartStore = create((set) => ({
 
 export default useCartStore;
 
-// get FavoriteCompany
-// export const getCartCount = async (user_id) => {
-
-//   const { setTotalCart } = useCartStore.getState();
-
-//   try {
-//    await connectMongo();
-//    const cart= await Cart.findOne({ user: user_id }).lean().exec();
-//    console.log("vnb",cart)
-
-//    setTotalCart(cart?.cartItems?.length ?? 0)
-
-//   } catch (err) {
-//     setTotalCart(0)
-//   }
-// };
 
